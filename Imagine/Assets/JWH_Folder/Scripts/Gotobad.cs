cffe96cb5b9e2536ee8d3296a04cc09a174388b1
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-04-21 01:53:48 +0900

    .
    .

--- Imagine/Assets/JWH_Folder/Scripts/Gotobad.cs
+++ Imagine/Assets/JWH_Folder/Scripts/Gotobad.cs
@@ -7,32 +7,24 @@ public class Gotobad : MonoBehaviour
 {
     public Transform transform1;
     public GameObject player;
-    private PlayerMoveMent playerMoveMent;
     public bool isCatch = false;
     private BoxCollider2D _boxCollider;
     private Rigidbody2D _rigidbody2D;
     private Transform plTransform;
     private float speed = 10f;
+    private LineRenderer lineRenderer;
     Vector3 movedir;
     private void Awake()
     {
-       
+        lineRenderer = GetComponent<LineRenderer>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _boxCollider = GetComponent<BoxCollider2D>();
     }
     private void Start()
     {
-        playerMoveMent = player.GetComponent<PlayerMoveMent>();
         plTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
     private void Update()
-    {
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("알ㄴ미루ㅏ미ㅏㅁ우류");
-        }*/
-    }
-    private void FixedUpdate()
     {
         Vector3 mousPos = Input.mousePosition;
         mousPos = Camera.main.ScreenToWorldPoint(mousPos);
@@ -40,16 +32,17 @@ public class Gotobad : MonoBehaviour
         Vector2 mosp = mousPos - transform1.position;
 
         
-
-        if (Input.GetButtonDown("Fire1") && isCatch)
+        if (Input.GetMouseButtonUp(0) && isCatch)
         {
-            
+
             Debug.Log("ssss");
             _boxCollider.enabled = true;
             //transform.up = mosp.normalized;
             _rigidbody2D.AddForce(mosp.normalized * speed, ForceMode2D.Impulse);
             isCatch = false;
+            
         }
+
 
         if (isCatch)
         {
@@ -67,6 +60,11 @@ public class Gotobad : MonoBehaviour
             _boxCollider.enabled = true;
             transform.SetParent(null);
         }
+
+    }
+    private void FixedUpdate()
+    {
+        
         
     }
     void PredictTrajectory(Vector3 startPos, Vector3 vel)
@@ -83,7 +81,7 @@ public class Gotobad : MonoBehaviour
             position += velocity * deltaTime + 0.5f * gravity * deltaTime * deltaTime;
             velocity += gravity * deltaTime;
 
-            print(position);
+            //print(position);
         }
     }
 
