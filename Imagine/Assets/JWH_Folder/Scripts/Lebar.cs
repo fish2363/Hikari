70bfb6e0815b6dbf7cea2f3bbf6ef91a4ee407fe
chuh007 <157554679+chuh007@users.noreply.github.com>
2024-05-01 17:16:39 +0900

    feat
    feat : Butten, Lebar, MoveingGround

--- Imagine/Assets/JWH_Folder/Scripts/Lebar.cs
+++ Imagine/Assets/JWH_Folder/Scripts/Lebar.cs
@@ -6,11 +6,27 @@ public class Lebar : MonoBehaviour
 {
     [SerializeField] private GameObject jakdong;
     public  bool isSwOn = false;
+    private Animator _animator;
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void Update()
     {
         if (isSwOn)
         {
             jakdong.SetActive(false);
+            _animator.SetBool("swOn",true);
         }
     }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("cusion"))
+        {
+            jakdong.SetActive(false);
+            _animator.SetBool("swOn", true);
+        }
+    }
+
 }
--- Imagine/Assets/JWH_Folder/Scripts/MoveingGround.cs
+++ Imagine/Assets/JWH_Folder/Scripts/MoveingGround.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.UIElements;
 
 public class MoveingGround : MonoBehaviour
 {
     [SerializeField] private float upDownSpeed = 0.01f;
     private bool wow = false;
+    [SerializeField]private Vector2 starttran;
+    private void Start()
+    {
+        starttran = transform.position;
+    }
     private void FixedUpdate()
     {
         if(wow) transform.position += Vector3.up * upDownSpeed;
+        else if (!wow && starttran.y < transform.position.y)
+        {
+            transform.position += Vector3.down * upDownSpeed;
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             wow = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            wow = false;
         }
     }
 }
